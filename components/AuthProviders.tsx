f60a89ca0484edd593a88a0579106cdf339eb104
wealcodersk <jahid.wealcoder@gmail.com>
2023-06-27 16:20:19 +0600

    working on auth provider

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,4 +1,21 @@
+"use client";
+
+import { getProviders, signIn} from 'next-auth/react';
+import {useState} from 'react'
+
+type Provider = {
+  id: string,
+  name: string,
+  type: string,
+  singleUrl: string,
+  callbackUrl: string,
+  signinUrlParams?: Record<string, string> | null;
+}
+
+type Providers = Record<string, Provider>;
+
 export default function AuthProviders() {
+  const [providers, setProviders] = useState<Providers | null>(null)
   return (
     <div>
       <p>Provider</p>
